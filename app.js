b33553a443b1a8839ce647c4111971308ef9939a
Vicente Gonzalez <johndoe@example.com>
2020-12-02 15:29:27 -0300

    mostrar todas las preguntas

--- app.js
+++ app.js
@@ -46,20 +46,30 @@ const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
 const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
 
-let currentQuestion = 0;
+let currentQuiz = 0;
 
 loadQuiz()
 
 function loadQuiz(){
-    const currentQuiz = quizData[currentQuestion]
+    const currentQuizData = quizData[currentQuiz]
 
-    questionEl.innerHTML = currentQuiz.question
+    questionEl.innerHTML = currentQuizData.question
 
-    a_text.innerText = currentQuiz.a
-    b_text.innerText = currentQuiz.b
-    c_text.innerText = currentQuiz.c
-    d_text.innerText = currentQuiz.d
-
-    currentQuestion++
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
 }
+
+submitBtn.addEventListener("click", () => {
+    currentQuiz++
+
+    if(currentQuiz < quizData.length){
+        loadQuiz()
+    } else {
+        //TODO: show results
+        alert("You finished ! Get yourself an Orange Lemonade")
+    }
+})
